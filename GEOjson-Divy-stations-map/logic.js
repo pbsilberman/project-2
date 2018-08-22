c8a4e27b042757b57ea3c31fcdb3f0c49b32cc6c
Jeff Rosal <jeros808@aol.com>
2018-08-21 19:09:49 -0500

    add total dock text

--- GEOjson-Divy-stations-map/logic.js
+++ GEOjson-Divy-stations-map/logic.js
@@ -4,20 +4,20 @@
     center: [
       41.826, -87.615 
     ],
-    zoom: 10,
+    zoom: 11,
   });
 
    L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     minZoom:2,
     maxZoom: 18,
-    id: "mapbox.light",
+    id: "mapbox.scenic",
     accessToken: API_KEY
   }).addTo(map);
 
 function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.station +
-      "</h3><hr><p>" + feature.properties.totaldocks + "</p>");
+      "</h3><hr><p>" + "Total Docks" + feature.properties.totaldocks + "</p>");
   }
 
    L.geoJSON(stations, {
